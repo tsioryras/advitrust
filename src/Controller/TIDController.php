a9d7add74e6984f55842d530f655f9afd1ef403c
uvsq21705895 <tsioryrasamoely@gmail.com>
2019-05-23 15:14:12 +0200

    finition

--- src/Controller/TIDController.php
+++ src/Controller/TIDController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TIDController extends AbstractController
@@ -15,5 +17,23 @@ class TIDController extends AbstractController
         return $this->render('tid/index.html.twig', [
             'controller_name' => 'TIDController',
         ]);
+    }
+
+    /**
+    * @Route("/rules", name="rules_file")
+    **/
+        public function downloadRules(){
+            $response = new BinaryFileResponse('path/to/../templates/reglement_TID.docx');
+            $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,'reglement_TID.docx');
+            return $response;
+        }
+
+    /**
+     * @Route("/form", name="form_file")
+     **/
+    public function downloadForm(){
+        $response = new BinaryFileResponse('path/to/../templates/formulaire_TID.docx');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,'reglement_TID.docx');
+        return $response;
     }
 }
